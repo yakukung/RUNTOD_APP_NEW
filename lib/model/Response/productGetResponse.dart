4503e498c1319078b33331ab8924d2a6f76a6da8
yakukung <yakukung2003@gmail.com>
2024-10-24 18:03:36 +0700

    อัพเดตเอา image_status ออก

--- lib/model/Response/productGetResponse.dart
+++ lib/model/Response/productGetResponse.dart
@@ -4,50 +4,50 @@
 
 import 'dart:convert';
 
-List<ProductGetResponse> productGetResponseFromJson(String str) => List<ProductGetResponse>.from(json.decode(str).map((x) => ProductGetResponse.fromJson(x)));
+List<ProductGetResponse> productGetResponseFromJson(String str) =>
+    List<ProductGetResponse>.from(
+        json.decode(str).map((x) => ProductGetResponse.fromJson(x)));
 
-String productGetResponseToJson(List<ProductGetResponse> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+String productGetResponseToJson(List<ProductGetResponse> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 class ProductGetResponse {
-    int orderItemId;
-    int orderId;
-    String senderId;
-    String nameItem;
-    String detailItem;
-    String imageProduct;
-    String imageStatus;
-    String createdDate;
+  int orderItemId;
+  int orderId;
+  String senderId;
+  String nameItem;
+  String detailItem;
+  String imageProduct;
+  String createdDate;
 
-    ProductGetResponse({
-        required this.orderItemId,
-        required this.orderId,
-        required this.senderId,
-        required this.nameItem,
-        required this.detailItem,
-        required this.imageProduct,
-        required this.imageStatus,
-        required this.createdDate,
-    });
+  ProductGetResponse({
+    required this.orderItemId,
+    required this.orderId,
+    required this.senderId,
+    required this.nameItem,
+    required this.detailItem,
+    required this.imageProduct,
+    required this.createdDate,
+  });
 
-    factory ProductGetResponse.fromJson(Map<String, dynamic> json) => ProductGetResponse(
+  factory ProductGetResponse.fromJson(Map<String, dynamic> json) =>
+      ProductGetResponse(
         orderItemId: json["order_item_id"],
         orderId: json["order_id"],
         senderId: json["sender_id"],
         nameItem: json["name_item"],
         detailItem: json["detail_item"],
         imageProduct: json["image_product"],
-        imageStatus: json["image_status"],
         createdDate: json["created_date"],
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "order_item_id": orderItemId,
         "order_id": orderId,
         "sender_id": senderId,
         "name_item": nameItem,
         "detail_item": detailItem,
         "image_product": imageProduct,
-        "image_status": imageStatus,
         "created_date": createdDate,
-    };
+      };
 }
